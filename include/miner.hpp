632ca850bafe07f148cb71bf28cf6b5c1e539416
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2022-08-21 00:49:14 -0500

    new executible CosmosWallet with very basic wallet functionality. New BoostMiner command boost, which both boosts and mines.

--- include/miner.hpp
+++ include/miner.hpp
@@ -3,6 +3,7 @@
 
 #include <gigamonkey/boost/boost.hpp>
 #include <whatsonchain_api.hpp>
+#include <random.hpp>
 
 using namespace Gigamonkey;
 
@@ -34,20 +35,44 @@ struct boost_prevouts {
     
 };
 
-// select a puzzle optimally. 
-boost_prevouts select(map<digest256, boost_prevouts> puzzles, double minimum_profitability = 0);
-
-bytes mine(
-    // an unredeemed Boost PoW output 
-    Boost::puzzle puzzle, 
-    // the address you want the bitcoins to go to once you have redeemed the boost output.
-    // this is not the same as 'miner address'. This is just an address in your 
-    // normal wallet and should not be the address that goes along with the key above.
-    Bitcoin::address address, 
-    // max time to mine before the function returns. Default is one full day. 
-    double max_time_seconds = 86400);
-
 Boost::puzzle prevouts_to_puzzle(const boost_prevouts &prevs, const Bitcoin::secret &key);
+
+struct miner {
+    BoostPOW::casual_random Random;
+    
+    // select a puzzle optimally. 
+    boost_prevouts select(map<digest256, boost_prevouts> puzzles, double minimum_profitability = 0);
+
+    Bitcoin::transaction mine(
+        // an unredeemed Boost PoW output 
+        Boost::puzzle puzzle, 
+        // the address you want the bitcoins to go to once you have redeemed the boost output.
+        // this is not the same as 'miner address'. This is just an address in your 
+        // normal wallet and should not be the address that goes along with the key above.
+        Bitcoin::address address, 
+        // max time to mine before the function returns. Default is one full day. 
+        double max_time_seconds = 86400);
+    
+};
+/*
+struct miner_outer {work::puzzle &
+    virtual void update_job(int, const work::puzzle &) = 0;
+};
+
+struct miner_inner {
+    // get latest job. If there is no job yet, block. 
+    // pointer will be null if the thread is supposed to stop. 
+    virtual const data::entry<int, work::puzzle> *latest_job() = 0;
+    virtual void solved(int, const work::solution &) = 0;
+};
+
+void mining_thread(miner_inner const *);
+
+struct miner : miner_outer, miner_inner {
+    std::shared_mutex Mutex;
+    std::list<data::entry<int, work::puzzle>> Puzzles;
+    int Max;
+};*/
 
 #endif
 
---
+++ src/cosmos.cpp
@@ -0,0 +1,269 @@
+
+#include <random.hpp>
+#include <wallet.hpp>
+#include <logger.hpp>
+#include <miner.hpp>
+#include <network.hpp>
+
+int command_generate(int arg_count, char** arg_values) {
+    if (arg_count != 1) throw "invalid number of arguments; one expected.";
+    
+    std::string filename{arg_values[0]};
+    
+    std::cout << "Type random characters and we will generate a wallet for you. Press enter when you think you have enough." << std::endl;
+    
+    std::string user_input{};
+    
+    while(true) {
+        char x = std::cin.get();
+        if (x == '\n') break;
+        user_input.push_back(x);
+    } 
+    
+    digest512 bits = SHA2_512(user_input);
+    
+    secp256k1::secret secret;
+    
+    hd::chain_code chain_code(32);
+    
+    std::copy(bits.begin(), bits.begin() + 32, secret.Value.begin());
+    std::copy(bits.begin() + 32, bits.end(), chain_code.begin());
+    
+    hd::bip32::secret master{secret, chain_code, hd::bip32::main};
+    
+    write_to_file(wallet{{}, master, 0}, filename);
+    return 0;
+}
+
+int command_receive(int arg_count, char** arg_values) {
+    if (arg_count != 1) throw "invalid number of arguments; one expected.";
+    
+    std::string filename{arg_values[0]};
+    auto w = read_wallet_from_file(filename);
+    
+    Bitcoin::secret new_key = Bitcoin::secret(w.Master.derive(w.Index));
+    
+    w.Index += 1;
+    
+    std::cout << new_key << " " << new_key.address() << std::endl;
+    
+    write_to_file(w, filename);
+    return 0;
+}
+
+int command_import(int arg_count, char** arg_values) {
+    if (arg_count != 5) throw std::string{"invalid number of arguments; five expected."};
+    
+    std::string filename{arg_values[0]};
+    
+    string arg_txid{arg_values[1]};
+    string arg_index{arg_values[2]};
+    string arg_value{arg_values[3]};
+    string arg_wif{arg_values[4]};
+    
+    digest256 txid{arg_txid};
+    
+    uint32 index;
+    std::stringstream{arg_index} >> index;
+    
+    int64 value;
+    std::stringstream{arg_value} >> value;
+    
+    Bitcoin::secret key{arg_wif};
+    if (!key.valid()) throw "could not read secret key";
+    
+    auto w = read_wallet_from_file(filename);
+    
+    w = w.add(p2pkh_prevout{txid, index, Bitcoin::satoshi{value}, key});
+    
+    write_to_file(w, filename);
+    return 0;
+}
+
+int command_send(int arg_count, char** arg_values) {
+    if (arg_count != 3) throw std::string{"invalid number of arguments; three expected."};
+    
+    std::string filename{arg_values[0]};
+    auto w = read_wallet_from_file(filename);
+    
+    write_to_file(w, filename);
+    return 0;
+}
+
+int command_value(int arg_count, char** arg_values) {
+    if (arg_count != 1) throw "invalid number of arguments; one expected.";
+    
+    std::string filename{arg_values[0]};
+    std::cout << read_wallet_from_file(filename).value() << std::endl;
+    
+    return 0;
+}
+
+int command_boost(int arg_count, char** arg_values) {
+    
+    if (arg_count < 4 || arg_count > 7) throw std::string{"invalid number of arguments; should at least 4 and at most 7"};
+    
+    std::string filename{arg_values[0]};
+    auto w = read_wallet_from_file(filename);
+    
+    string arg_value{arg_values[1]};
+    
+    int64 satoshi_value;
+    std::stringstream{arg_value} >> satoshi_value;
+    
+    Bitcoin::satoshi value = satoshi_value;
+    
+    if (value > w.value()) throw "insufficient funds";
+    
+    string arg_content{arg_values[2]};
+    
+    digest256 content{arg_content};
+    if (!content.valid()) throw (string{"could not read content: "} + arg_content);
+    
+    double diff = 0;
+    string difficulty_input{arg_values[3]};
+    std::stringstream diff_stream{difficulty_input};
+    diff_stream >> diff;
+    
+    work::compact target{work::difficulty{diff}};
+    if (!target.valid()) throw (std::string{"could not read difficulty: "} + difficulty_input);
+    
+    bytes topic{};
+    bytes additional_data{};
+    
+    if (arg_count > 4) {
+    
+        ptr<bytes> topic_read = encoding::hex::read(string{arg_values[4]});
+        if (topic_read == nullptr || topic_read->size() > 20) throw (std::string{"could not read topic: "} + string{arg_values[4]});
+        topic = *topic_read;
+    }
+    
+    if (arg_count > 5) {
+        ptr<bytes> add_data = encoding::hex::read(string{arg_values[5]});
+        if (add_data == nullptr) throw (std::string{"could not read additional_data: "} + string{arg_values[5]});
+        additional_data = *add_data;
+    }
+    
+    Boost::type type = Boost::bounty;
+    
+    if (arg_count > 6) {
+        string boost_type{arg_values[6]};
+        if (boost_type == "contract") type = Boost::contract;
+        else if (boost_type != "bounty") throw (std::string{"could not boost type: "} + string{arg_values[6]});
+        
+    }
+    
+    if (type == Boost::contract) throw std::string{"We do not do boost contract yet"};
+    
+    Boost::output_script boost_output_script = Boost::output_script::bounty(
+        0, content, target, topic, 
+        BoostPOW::casual_random{}.uint32(), 
+        additional_data, false);
+    
+    Boost::output boost_output{value, boost_output_script};
+    auto bitcoin_output = Bitcoin::output(boost_output);
+    
+    auto spend = w.spend(bitcoin_output);
+    
+    uint32 boost_output_index = 0;
+    // where is the output script? 
+    for (const Bitcoin::output &o : spend.Transaction.Outputs) {
+        if (o == bitcoin_output) break;
+        boost_output_index++;
+    }
+    
+    if (boost_output_index == spend.Transaction.Outputs.size()) throw "could not find boost output index";
+    
+    network net{};
+    
+    net.broadcast(bytes(spend.Transaction));
+    
+    write_to_file(spend.Wallet, filename);
+    /*
+    w = spend.Wallet;
+    
+    Bitcoin::secret miner_key = Bitcoin::secret(w.Master.derive(w.Index));
+    Bitcoin::secret spend_key = Bitcoin::secret(w.Master.derive(w.Index + 1));
+    
+    w.Index += 2;
+    
+    Bitcoin::transaction redeem_tx = mine(
+        Boost::puzzle{{Boost::prevout{
+            Bitcoin::outpoint{spend.Transaction.id(), boost_output_index}, 
+            boost_output}}, miner_key}, spend_key.address());
+    
+    bytes redeem_tx_bytes = bytes(redeem_tx);
+    
+    net.broadcast(redeem_tx_bytes);
+    
+    w = w.add(p2pkh_prevout{
+        Bitcoin::Hash256(redeem_tx_bytes), 0, 
+        redeem_tx.Outputs[0].Value,
+        spend_key});*/
+    
+    write_to_file(w, filename);
+    
+    return 0;
+}
+
+int help() {
+
+    std::cout << "input should be \"function\" \"args\"... where function is "
+        "\n\tgenerate   -- create a new wallet."
+        "\n\tvalue      -- print the total value in the wallet."
+        "\n\treceive    -- generate a new address."
+        "\n\timport     -- add a utxo to this wallet."
+        "\n\tsend       -- send to an address or script."
+        "\n\tboost      -- boost content ."
+        "\nFor function \"generate\", remaining inputs should be "
+        "\n\tfilename   -- wallet file name. "
+        "\nFor function \"value\", remaining inputs should be "
+        "\n\tfilename   -- wallet file name. "
+        "\nFor function \"address\", remaining inputs should be "
+        "\n\tfilename   -- wallet file name. "
+        "\nFor function \"import\", remaining inputs should be "
+        "\n\tfilename   -- wallet file name. "
+        "\n\ttxid       -- txid of the tx that contains the output to be added to the wallet."
+        "\n\tindex      -- index of the output within that tx."
+        "\n\tvalue      -- value in satoshis of the output."
+        "\n\twif        -- private key that will be used to redeem this output."
+        "\nFor function \"send\", remaining inputs should be "
+        "\n\tfilename   -- wallet file name. "
+        "\n\tvalue      -- value in satoshis to send."
+        "\n\taddress    -- address or script (bip 276 encoded) to send them to." 
+        "\nFor function \"boost\", remaining inputs should be "
+        "\n\tfilename   -- wallet file name. "
+        "\n\tvalue      -- value in satoshis to pay for boost."
+        "\n\tcontent    -- hex for correct order, hexidecimal for reversed."
+        "\n\tdifficulty -- "
+        "\n\ttopic      -- OPTIONAL: string max 20 bytes."
+        "\n\tadd. data  -- OPTIONAL: string, any size."
+        "\n\ttype       -- OPTIONAL: may be either 'bounty' or 'contract'. Default is 'bounty'" << std::endl;
+    
+    return 0;
+}
+
+int main(int arg_count, char** arg_values) {
+    
+    if(arg_count == 1) return help();
+    //if (arg_count != 5) return help();
+    
+    string function{arg_values[1]};
+    
+    try {
+        if (function == "generate") return command_generate(arg_count - 2, arg_values + 2);
+        if (function == "import") return command_import(arg_count - 2, arg_values + 2);
+        if (function == "value") return command_value(arg_count - 2, arg_values + 2);
+        if (function == "receive") return command_receive(arg_count - 2, arg_values + 2);
+        if (function == "send") return command_send(arg_count - 2, arg_values + 2);
+        if (function == "boost") return command_boost(arg_count - 2, arg_values + 2);
+        if (function == "help") return help();
+        help();
+    } catch (std::string x) {
+        std::cout << "Error: " << x << std::endl;
+        return 1;
+    }
+    
+    return 0;
+}
+
