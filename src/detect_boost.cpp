7184fcdc6676ce91c2347dbd067199f53d990abd
Daniel Krawisz <danielkrawisz@protonmail.com>
2022-12-27 11:18:38 -0500

    Add program that detects txs with boost outputs

---
+++ src/detect_boost.cpp
@@ -0,0 +1,35 @@
+
+#include <data/encoding/hex.hpp>
+#include <gigamonkey/timechain.hpp>
+#include <jobs.hpp>
+
+using namespace Gigamonkey;
+
+int main(int arg_count, char** args) {
+    
+    try {
+        
+        if (arg_count != 2) throw data::exception {"Invalid number of arguments; expected one argument."};
+        
+        ptr<bytes> read_hex = encoding::hex::read(std::string{args[1]});
+        
+        if (read_hex == nullptr) throw data::exception {"expected hex string"};
+        
+        auto tx = Bitcoin::transaction{*read_hex};
+        if (!tx.valid()) throw data::exception {"could not read transaction"};
+        
+        for (auto &o : tx.Outputs) if (Boost::output_script{o.Script}.valid()) {
+            std::cout << BoostPOW::write(tx.id()) << std::endl;
+            return 0;
+        }
+        
+    } catch (data::exception &e) {
+        std::cout << "error: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cout << "error: unknown" << std::endl;
+        return 1;
+    }
+    
+    return 0;
+};
